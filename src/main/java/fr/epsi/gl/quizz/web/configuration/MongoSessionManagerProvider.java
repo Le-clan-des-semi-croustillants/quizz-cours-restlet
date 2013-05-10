c9de94d323be3d69c7c57d0a50e99d23d0982176
Bastienp <bastien.poidevain@gmail.com>
2013-05-10 20:43:52 +0200

    Commit Modif MongoSessionManager Fonctionne en local

--- src/main/java/fr/epsi/gl/quizz/web/configuration/MongoSessionManagerProvider.java
+++ src/main/java/fr/epsi/gl/quizz/web/configuration/MongoSessionManagerProvider.java
@@ -13,10 +13,10 @@ public class MongoSessionManagerProvider implements Provider<MongoSessionManager
     public MongoSessionManager get() {
         Settings settings = Settings.defaultInstance()
                 .withDefaultUpdateStrategy(UpdateStrategies.DIFF)
-                .withDbName("db")
+                .withDbName("lcdsct/QuizzDataBase")
                 .withHost("linus.mongohq.com")
                 .withPort(10027)
-                .withAuthentication("9951e0fe3d6eedbc40213112625ac63f", "rzG1TpwGpZDTFe4QS2HI9Q");
+                .withAuthentication("lcdsct", "lcdsct123");
 
 
         //return MongoSessionManager.create(new ContextBuilder("fr.epsi.gl.quizz.persistance.mongo.mapping"), Settings.defaultInstance().withDbName("quizz").withDefaultUpdateStrategy(UpdateStrategies.DIFF));
