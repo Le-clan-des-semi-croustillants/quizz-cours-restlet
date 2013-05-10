e56c781b72ad3cf5a74d4fa5e35bce8ea2a6b634
Bastienp <bastien.poidevain@gmail.com>
2013-05-10 21:52:59 +0200

    Commit Modif MongoSessionManager Fonctionne en local

--- src/main/java/fr/epsi/gl/quizz/web/configuration/MongoSessionManagerProvider.java
+++ src/main/java/fr/epsi/gl/quizz/web/configuration/MongoSessionManagerProvider.java
@@ -1,5 +1,6 @@
 package fr.epsi.gl.quizz.web.configuration;
 
+
 import org.mongolink.MongoSessionManager;
 import org.mongolink.Settings;
 import org.mongolink.domain.UpdateStrategies;
@@ -11,15 +12,20 @@ public class MongoSessionManagerProvider implements Provider<MongoSessionManager
 
     @Override
     public MongoSessionManager get() {
-        Settings settings = Settings.defaultInstance()
+        /*Settings settings = Settings.defaultInstance()
                 .withDefaultUpdateStrategy(UpdateStrategies.DIFF)
                 .withDbName("lcdsct/QuizzDataBase")
                 .withHost("linus.mongohq.com")
                 .withPort(10027)
                 .withAuthentication("lcdsct", "lcdsct123");
+         */
 
 
         //return MongoSessionManager.create(new ContextBuilder("fr.epsi.gl.quizz.persistance.mongo.mapping"), Settings.defaultInstance().withDbName("quizz").withDefaultUpdateStrategy(UpdateStrategies.DIFF));
-        return MongoSessionManager.create(new ContextBuilder("fr.epsi.gl.quizz.persistance.mongo.mapping"), settings);
+        //return MongoSessionManager.create(new ContextBuilder("fr.epsi.gl.quizz.persistance.mongo.mapping"), settings);
+        return MongoSessionManager.create(new ContextBuilder("fr.epsi.gl.quizz.persistance.mongo.mapping"),
+                Settings.defaultInstance()
+                        .withDbName("lcdsct/QuizzDataBase")
+                        .withDefaultUpdateStrategy(UpdateStrategies.DIFF));
     }
 }
